3b2897937820f0cc4b37dbf3fe46a702dd5b711e
Martha <mtamakloe44@gmail.com>
2023-03-14 23:22:24 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,14 +15,12 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
-
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
@@ -30,4 +28,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		conct[i] = s1[i];
 		i++;
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
 	}
+	conct[i] = '\0';
+	return (conct);
+}
